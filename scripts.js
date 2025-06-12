eae1a0eeab4c232ded2ecb6abafc910a741d29c8
MarianaASoares <mariluusoares@gmail.com>
2025-06-12 20:50:48 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function generateNumber(){
+
+    const inputMin = Math.ceil (document.querySelector(".input-min").value)
+    const inputMax = Math.floor(document.querySelector(".input-max").value) 
+   
+    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
+
+
+    alert(`O número sorteado é ${result}`)
+}
+
+
+
+
+
+
+
