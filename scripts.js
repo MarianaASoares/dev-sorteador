e85a893710527f66992248c71cd2dc24f3d357ae
MarianaASoares <mariluusoares@gmail.com>
2025-06-17 15:58:08 -0300

    Verificando valores mínimo e máximo

--- scripts.js
+++ scripts.js
@@ -2,11 +2,19 @@ function generateNumber(){
 
     const inputMin = Math.ceil (document.querySelector(".input-min").value)
     const inputMax = Math.floor(document.querySelector(".input-max").value) 
-   
-    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
 
+    if( inputMin >= inputMax){
+        alert("O valor mínimo deve ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
 
     alert(`O número sorteado é ${result}`)
+    }
+
+
+
+   
+    
 }
 
 
